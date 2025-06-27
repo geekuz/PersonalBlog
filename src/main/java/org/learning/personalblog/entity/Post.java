f820fe3e0efd927b582ba89d8a001dbc9d1a9ab9
otabekgeek <otabekgeek@gmail.com>
2025-06-27 15:50:18 +0500

    security_improvements

--- src/main/java/org/learning/personalblog/entity/Post.java
+++ src/main/java/org/learning/personalblog/entity/Post.java
@@ -3,8 +3,8 @@ package org.learning.personalblog.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
-import org.springframework.data.annotation.CreatedDate;
 
 import java.time.LocalDateTime;
 
@@ -13,13 +13,27 @@ import java.time.LocalDateTime;
 @Setter
 @Table(name = "posts")
 public class Post {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, length = 255)
     private String title;
+
+    @Lob
+    @Column(nullable = false, columnDefinition = "TEXT")
     private String content;
-    @CreatedDate
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
     private LocalDateTime createdAt;
+
     @UpdateTimestamp
+    @Column(nullable = false)
     private LocalDateTime updatedAt;
 }
--- src/main/java/org/learning/personalblog/entity/Role.java
+++ src/main/java/org/learning/personalblog/entity/Role.java
@@ -1,4 +1,25 @@
 package org.learning.personalblog.entity;
 
-public class Role {
+
+import org.springframework.security.core.GrantedAuthority;
+
+/// **
+// * Defines the roles available in the application.
+// * Implements GrantedAuthority to be used directly by Spring Security.
+// */
+public enum Role implements GrantedAuthority {
+    READER,
+    CREATOR;
+
+    /**
+     * Returns the authority string that Spring Security uses for role-based authorization.
+     * For example, for the ADMIN role, this will return "ROLE_ADMIN".
+     *
+     * @return The string representation of the authority.
+     */
+    @Override
+    public String getAuthority() {
+        // The "ROLE_" prefix is a convention for Spring Security.
+        return "ROLE_" + this.name();
+    }
 }
